17536851cd40cdb25c374e6d293ddf2d8a81ccd6
hasanposh <hmasif147@gmail.com>
2024-04-08 03:06:33 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbP7vxXpuh_m8UMWbBKppaioa3kK2ODlw",
+  authDomain: "real-estate-with-react.firebaseapp.com",
+  projectId: "real-estate-with-react",
+  storageBucket: "real-estate-with-react.appspot.com",
+  messagingSenderId: "550965427652",
+  appId: "1:550965427652:web:e793059b4dbb98ef07b891"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
